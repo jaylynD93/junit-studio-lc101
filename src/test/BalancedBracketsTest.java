be47079a1d8317d1550d09dadd9b1f97fbba4164
jaylyn dunlap <j.dunlap@aol.com>
2021-01-25 19:57:34 -0600

    FINISHED

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,52 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
-
+    @Test
+   public void initTest(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void twoTest(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+    @Test
+    public void threeTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+    @Test
+    public void fourTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+    @Test
+    public void fiveTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]"));
+    }
+    @Test
+    public void sixTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LAUN]CH]"));
+    }
+    @Test
+    public void sevenTest(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][]"));
+    }
+    @Test
+    public void eightTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LA[UN]CH]]"));
+    }
+    @Test
+    public void nineTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+    @Test
+    public void tenTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]]"));
+    }
+    @Test
+    public void elevenTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][]]"));
+    }
+    @Test
+    public void twelveTest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]"));
+    }
 }
